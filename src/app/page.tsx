43de0b825399c9ea32ec7e66a5d2b5e2b5eda79f
Gustavo-Nasc <gustavo.ns@outlook.com.br>
2024-09-26 11:28:13 -0300

    feat: starting static login page construction

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,18 @@
 import Image from 'next/image'
 import logo from '@/assets/logo.svg'
-import { Button } from '@/components/Button'
-import { Play } from 'lucide-react'
+import shapeLogin from '@/assets/shape-login.png'
 
-export default function Home() {
+export default function Login() {
   return (
-    <section className="flex flex-col gap-4 w-screen h-screen items-center justify-center">
+    <section className="relative flex flex-col gap-4 w-screen h-screen items-center justify-center">
+      <Image
+        src={shapeLogin}
+        alt="Shape Login"
+        className="absolute -left-36 -top-36 w-1/2 -z-10"
+      />
       <Image src={logo} alt="Logo Click" />
-      <h1 className="text-4xl font-bold">Hello Click!</h1>
-      <Button className="bg-white hover:bg-neutral-300 text-black font-secondary">
-        <span className="">Começar</span>
-        <Play color="black" fill="black" />
-      </Button>
+      <h1 className="text-4xl font-bold uppercase">Click!</h1>
+      <form></form>
     </section>
   )
 }
